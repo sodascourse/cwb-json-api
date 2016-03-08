5324c3f335a9c0bb1b8d6265f42d8522219d9511
sodastsai <sodas2002@gmail.com>
2016-03-08 16:25:47 +0800

    Discover `pages` package to load blueprint pages during launch

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
 import os
+import pathlib
+from importlib import import_module
 
-from flask import Flask
+from flask import Flask, jsonify, Blueprint
+
+source_root = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def root():
+    return jsonify(
+        name="CWB JSON API",
+        source="https://github.com/sodascourse/cwb-json-api",
+        endpoints=[url_rule.rule for url_rule in app.url_map.iter_rules()
+                   if not url_rule.rule.startswith('/static') and url_rule.rule != '/'],
+    )
+
+
+# Find pages
+for page_module_path in pathlib.Path(source_root, 'pages').glob('*.py'):
+    page_module_name = page_module_path.stem
+    if page_module_name == '__init__':
+        continue
+
+    try:
+        page_module = import_module('pages.{}'.format(page_module_name))
+    except ImportError:
+        continue
+
+    blueprint_page = getattr(page_module, 'page', None)
+    if blueprint_page is None or not isinstance(blueprint_page, Blueprint):
+        continue
+
+    url_prefix = getattr(page_module, 'url_prefix', None)
+    if url_prefix is None or not isinstance(url_prefix, str):
+        continue
+
+    app.register_blueprint(blueprint_page, url_prefix=url_prefix)
+
 
 if __name__ == "__main__":
     DEBUG = int(os.environ.get('DEBUG', '0')) != 0
